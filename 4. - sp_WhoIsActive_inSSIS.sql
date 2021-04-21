99dc3d3088b5977c7d4a3d6e3957697dd302a151
Marek Grzymala <marek.grzymala@grandparade.co.uk>
2021-04-21 13:41:53 +0200

    v.0.02 - parameterized: @ProjectsToIgnore NVARCHAR(MAX) @PackagesToIgnore NVARCHAR(MAX)

--- 4. - sp_WhoIsActive_inSSIS.sql
+++ 4. - sp_WhoIsActive_inSSIS.sql
@@ -21,14 +21,18 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE OR ALTER PROCEDURE [dbo].[sp_WhoIsActive_inSSIS]
-       @PercentileLower FLOAT = 0.2, /* percentile BELOW WHICH we are excluding the outliers from the stats calculation */
-       @PercentileUpper FLOAT = 0.8, /* percentile ABOVE WHICH we are excluding the outliers from the stats calculation */
-       @DaysBack INT = -30, /* the range (in days) of how far back we are calculating the duration/start-time statistics */
-       @DaysMargin INT = 10 /* if a package execution is missing some days within the range of @DaysBack 
-                               but never runs MORE THAN ONCE DAILY this is the max. number of skipped days 
-                               still allowed to calculate the StartTime Avg, Lower/Upper Percentiles etc. for that package */
+       
+        @PercentileLower FLOAT = 0.2         /* percentile BELOW WHICH we are excluding the outliers from the stats calculation */
+       ,@PercentileUpper FLOAT = 0.8         /* percentile ABOVE WHICH we are excluding the outliers from the stats calculation */
+       ,@DaysBack INT = -30                  /* the range (in days) of how far back we are calculating the duration/start-time statistics */
+       ,@DaysMargin INT = 10                 /* if a package execution is missing some days within the range of @DaysBack 
+                                               but never runs MORE THAN ONCE DAILY this is the max. number of skipped days 
+                                               still allowed to calculate the StartTime Avg, Lower/Upper Percentiles for that package */
+       ,@ProjectsToIgnore NVARCHAR(MAX) = 'Dyn_Dyn_Orchestration'                   /* comma-separated names of projects you want to ignore in the list */
+       ,@PackagesToIgnore NVARCHAR(MAX) = 'Orchestration.dtsx, ParallelStream.dtsx' /* comma-separated names of packages you want to ignore in the list */
 AS
 BEGIN
+
 
 SELECT
              ei.execution_id
@@ -38,20 +42,32 @@ SELECT
             ,CAST(ei.start_time AS DATETIME2(0))                               AS [StartTime]
 
             /* Start-Time Results: */
+            /* if a package execution is missing @DaysMargin number of days within the range of @DaysBack 
+               but never runs MORE THAN ONCE DAILY calculate the StartTime Avg, Lower/Upper Percentiles for that package */
             ,CASE WHEN 
                  (rd.[RunsOnceDaily] = 1) OR (rd.[RunsOnceDaily] = 0 AND rd.[RanMoreThanOnceDaily] = 0 AND rd.[HowManyTimesDidItRun] >= (@DaysBack*(-1))-@DaysMargin) 
                  THEN hstavg.[StartTimeAvg]   ELSE NULL
-             END AS [StartTimeAvg]
-
+             END                                                                           AS [StartTimeAvg]
             ,CASE WHEN 
                  (rd.[RunsOnceDaily] = 1) OR (rd.[RunsOnceDaily] = 0 AND rd.[RanMoreThanOnceDaily] = 0 AND rd.[HowManyTimesDidItRun] >= (@DaysBack*(-1))-@DaysMargin) 
                  THEN DATEDIFF(MINUTE, CAST(ei.start_time AS TIME(0)), hstavg.[StartTimeAvg])   
                  ELSE NULL
-             END AS [StartTime Behind/AheadOf Avg]
-            ,prcnt.[TimeStart_PercLower]
-            ,prcnt.[TimeStart_PercUpper]
-            ,prcnt.[TimeEnd_PercLower]
-            ,prcnt.[TimeEnd_PercUpper]
+             END                                                                          AS [StartTime Behind/AheadOf Avg]
+            ,CASE WHEN 
+                  (rd.[RunsOnceDaily] = 1) OR (rd.[RunsOnceDaily] = 0 AND rd.[RanMoreThanOnceDaily] = 0 AND rd.[HowManyTimesDidItRun] >= (@DaysBack*(-1))-@DaysMargin) 
+                  THEN prcnt.[TimeStart_PercLower] ELSE NULL 
+             END                                                                          AS [TimeStart_PercLower]
+            ,CASE WHEN 
+                  (rd.[RunsOnceDaily] = 1) OR (rd.[RunsOnceDaily] = 0 AND rd.[RanMoreThanOnceDaily] = 0 AND rd.[HowManyTimesDidItRun] >= (@DaysBack*(-1))-@DaysMargin) 
+                  THEN prcnt.[TimeStart_PercUpper] ELSE NULL 
+             END                                                                          AS [TimeStart_PercUpper]
+            ,CASE WHEN 
+                  (rd.[RunsOnceDaily] = 1) OR (rd.[RunsOnceDaily] = 0 AND rd.[RanMoreThanOnceDaily] = 0 AND rd.[HowManyTimesDidItRun] >= (@DaysBack*(-1))-@DaysMargin) 
+                  THEN prcnt.[TimeEnd_PercLower]   ELSE NULL 
+             END                                                                          AS [TimeEnd_PercLower]  
+            ,CASE WHEN (rd.[RunsOnceDaily] = 1) OR (rd.[RunsOnceDaily] = 0 AND rd.[RanMoreThanOnceDaily] = 0 AND rd.[HowManyTimesDidItRun] >= (@DaysBack*(-1))-@DaysMargin) 
+                  THEN prcnt.[TimeEnd_PercUpper]   ELSE NULL 
+             END                                                                          AS [TimeEnd_PercUpper]
             ,CAST(DATEADD(MINUTE, hstavg.[DurationAvgMinutes], ei.start_time) AS TIME(0)) AS [ExpctEndTime]
 
             /* Duration Results: */
@@ -97,7 +113,9 @@ OUTER APPLY (
             )   AS      hstavg
 
 WHERE      ei.status = 2 -- Currently Running
-AND        ei.package_name NOT IN ('Orchestration.dtsx', 'ParallelStream.dtsx')
+AND        ei.project_name NOT IN (SELECT LTRIM([value]) FROM STRING_SPLIT(@ProjectsToIgnore, ','))
+AND        ei.package_name NOT IN (SELECT LTRIM([value]) FROM STRING_SPLIT(@PackagesToIgnore, ','))
+
 ORDER BY   ei.execution_id DESC 
 END
 GO
