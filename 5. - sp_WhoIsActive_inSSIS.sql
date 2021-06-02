31c47862df18d879a4de1e94d9f4434934e4213d
Marek Grzymala <marek.grzymala@grandparade.co.uk>
2021-06-02 11:48:25 +0200

    v0.04 - replaced all functions by buitl-in queries matching the functions (eliminated the need to define the functions separately)

--- 5. - sp_WhoIsActive_inSSIS.sql
+++ 5. - sp_WhoIsActive_inSSIS.sql
@@ -1,5 +1,5 @@
 /*********************************************************************************************
-sp_WhoIsActive_inSSIS v0.03 (2021-04-21)
+sp_WhoIsActive_inSSIS v0.04 (2021-06-02)
 (C) 2021, Marek Grzymala
 
 Feedback: https://www.linkedin.com/in/marek-grzymala-sql/
@@ -33,7 +33,7 @@ CREATE OR ALTER PROCEDURE [dbo].[sp_WhoIsActive_inSSIS]
 AS
 BEGIN
 
-
+DECLARE @EndDate DATETIME = GETDATE() /* change if you want to limit the stats calculation to an earlier date in the past */
 SELECT
              ei.execution_id
             ,ei.project_name
@@ -82,40 +82,98 @@ SELECT
             ,rd.[HowManyTimesDidItRun]
 
 FROM        [SSISDB].internal.execution_info ei
+/* ----------------------------- Function to GetPackageHistPercentiles_PerPackageName: ----------------------------- */
 OUTER APPLY (
-                SELECT 
-                        [ProjectName],
-                        [PackageName],
-                        [TimeStart_PercLower],
-                        [TimeStart_PercUpper],
-                        [TimeEnd_PercLower],
-                        [TimeEnd_PercUpper],
-                        [Duration_PercLower],
-                        [Duration_PercUpper] 
-                FROM    dbo.ufn_GetPackageHistPercentiles_PerPackageName(ei.project_name, ei.package_name, @PercentileLower, @PercentileUpper, @DaysBack, GETDATE())
-            )   AS      prcnt
+            SELECT  TOP 1 
+            
+                     ei_prcnt.project_name AS [ProjectName]
+                    ,ei_prcnt.package_name AS [PackageName]
+            
+                    ,CAST(DATEADD(SECOND, PERCENTILE_CONT(@PercentileLower) WITHIN GROUP (ORDER BY DATEDIFF(SECOND, CAST(CAST(ei_prcnt.start_time AS DATE) AS DATETIME), CAST(ei_prcnt.start_time AS DATETIME))) OVER (PARTITION BY ei_prcnt.package_name), 0) AS TIME(0)) AS [TimeStart_PercLower]
+                    ,CAST(DATEADD(SECOND, PERCENTILE_CONT(@PercentileUpper) WITHIN GROUP (ORDER BY DATEDIFF(SECOND, CAST(CAST(ei_prcnt.start_time AS DATE) AS DATETIME), CAST(ei_prcnt.start_time AS DATETIME))) OVER (PARTITION BY ei_prcnt.package_name), 0) AS TIME(0)) AS [TimeStart_PercUpper]
+                    
+                    ,CAST(DATEADD(SECOND, PERCENTILE_CONT(@PercentileLower) WITHIN GROUP (ORDER BY DATEDIFF(SECOND, CAST(CAST(ei_prcnt.end_time AS DATE) AS DATETIME), CAST(ei_prcnt.end_time AS DATETIME))) OVER (PARTITION BY ei_prcnt.package_name), 0) AS TIME(0)) AS [TimeEnd_PercLower]
+                    ,CAST(DATEADD(SECOND, PERCENTILE_CONT(@PercentileUpper) WITHIN GROUP (ORDER BY DATEDIFF(SECOND, CAST(CAST(ei_prcnt.end_time AS DATE) AS DATETIME), CAST(ei_prcnt.end_time AS DATETIME))) OVER (PARTITION BY ei_prcnt.package_name), 0) AS TIME(0)) AS [TimeEnd_PercUpper]
+            
+                    ,PERCENTILE_CONT(@PercentileLower) WITHIN GROUP (ORDER BY DATEDIFF(MINUTE, ei_prcnt.start_time, ei_prcnt.end_time)) OVER (PARTITION BY ei_prcnt.package_name) AS [Duration_PercLower]
+                    ,PERCENTILE_CONT(@PercentileUpper) WITHIN GROUP (ORDER BY DATEDIFF(MINUTE, ei_prcnt.start_time, ei_prcnt.end_time)) OVER (PARTITION BY ei_prcnt.package_name) AS [Duration_PercUpper]
+            
+            FROM    [SSISDB].internal.execution_info ei_prcnt WITH (NOLOCK)           
+            WHERE   1 = 1
+            AND     ei_prcnt.project_name = ei.project_name
+            AND     ei_prcnt.package_name = ei.package_name
+            AND     ei_prcnt.status = 7 /* Success (we want to calculate stats on successfull runs only)  */ 
+            AND     ei_prcnt.start_time >= DATEADD(DAY, @DaysBack, CAST(CAST(@EndDate+1 AS DATE) AS DATETIME)) /* cast the date to midnight next day minus @DaysBack */ 
+            AND     ei_prcnt.end_time < CAST(CAST(@EndDate+1 AS DATE) AS DATETIME) /* cast the date to midnight next day */ 
+            ) AS    prcnt
+/* ----------------------------- Function to find if the PackageRunsOnceDaily: ----------------------------- */
 OUTER APPLY (
-                SELECT
-                        [ProjectName],
-                        [PackageName],
-                        [RunsOnceDaily],
-                        [RanMoreThanOnceDaily],
-                        [HowManyTimesDidItRun]
-                FROM    dbo.ufn_DoesPackageRunOnceDaily(ei.project_name, ei.package_name, @DaysBack, GETDATE()) 
-            )   AS      rd
+            SELECT 
+                      DistCnt.project_name AS [ProjectName],
+                      DistCnt.package_name AS [PackageName],
+                      CASE 
+                          WHEN ((SUM(DistCnt.DistinctCountOfExecIdsPerDay) = @DaysBack  * (-1)) AND MAX(DistCnt.DistinctCountOfExecIdsPerDay) = 1) THEN 1
+                          ELSE 0
+                      END AS [RunsOnceDaily],
+                      CASE 
+                          WHEN (MAX(DistCnt.DistinctCountOfExecIdsPerDay) > 1) THEN 1
+                          ELSE 0
+                      END AS [RanMoreThanOnceDaily],
+                      SUM(DistCnt.DistinctCountOfExecIdsPerDay) AS [HowManyTimesDidItRun]
+            FROM      (
+                          SELECT  
+                                    CAST(ei_rd.start_time AS DATE) AS [Date]
+                                   ,COUNT(DISTINCT ei_rd.execution_id) AS [DistinctCountOfExecIdsPerDay]
+                                   ,ei_rd.project_name
+                                   ,ei_rd.package_name
+                          FROM     [SSISDB].internal.execution_info ei_rd WITH (NOLOCK)           
+                          WHERE    1 = 1
+                          AND      ei_rd.project_name = ei.project_name
+                          AND      ei_rd.package_name = ei.package_name
+                          AND      ei_rd.start_time >= DATEADD(DAY, @DaysBack , GETDATE())
+                          AND      ei_rd.end_time <= @EndDate
+                          AND      ei_rd.status = 7 -- Success (we want to calculate stats on successfull runs only)  
+                          GROUP BY CAST(ei_rd.start_time AS DATE), ei_rd.project_name, ei_rd.package_name
+                      )   AS DistCnt
+            GROUP BY               
+                      DistCnt.project_name,
+                      DistCnt.package_name
+            ) AS      rd
+/* ----------------------------- Function to GetPackageHistAvg_Duration_PerPackageName_ExclOutliers: ----------------------------- */
 OUTER APPLY (
-                SELECT 
-                         [ProjectName]
-                        ,[PackageName]
-                        ,[DurationAvgMinutes]
-                FROM    dbo.ufn_GetPackageHistAvg_Duration_PerPackageName_ExclOutliers(ei.project_name, ei.package_name, prcnt.Duration_PercLower, prcnt.Duration_PercUpper, @DaysBack, GETDATE())
-            )   AS      hstavg_duration
+            SELECT       
+                         ei.project_name AS [ProjectName]
+                        ,ei.package_name AS [PackageName]
+                        ,AVG(DATEDIFF(MINUTE, ei_hst_av_dur.start_time, ei_hst_av_dur.end_time)) AS [DurationAvgMinutes]
+            FROM        [SSISDB].internal.execution_info ei_hst_av_dur WITH (NOLOCK)
+            
+            WHERE       1 = 1
+            AND         ei_hst_av_dur.project_name = ei.project_name
+            AND         ei_hst_av_dur.package_name = ei.package_name
+            AND         ei_hst_av_dur.status = 7 /* Success (we want to calculate stats on successfull runs only)   */ 
+            AND         ei_hst_av_dur.start_time >= DATEADD(DAY, @DaysBack, CAST(CAST(@EndDate+1 AS DATE) AS DATETIME)) /* Cast the date to midnight next day minus @DaysBack */ 
+            AND         ei_hst_av_dur.end_time < CAST(CAST(@EndDate+1 AS DATE) AS DATETIME) /* CAST the date to midnight next day */
+            /* Here we are excluding duration outliers: */
+            AND         DATEDIFF(MINUTE, ei_hst_av_dur.start_time, ei_hst_av_dur.end_time) >= prcnt.Duration_PercLower
+            AND         DATEDIFF(MINUTE, ei_hst_av_dur.start_time, ei_hst_av_dur.end_time) <= prcnt.Duration_PercUpper
+)   AS      hstavg_duration
+/* ----------------------------- Function to GetPackageHistAvg_StartTime_PerPackageName_ExclOutliers: ----------------------------- */
 OUTER APPLY (
-                SELECT 
-                         [ProjectName]
-                        ,[PackageName]
-                        ,[StartTimeAvg]
-                FROM    dbo.ufn_GetPackageHistAvg_StartTime_PerPackageName_ExclOutliers(ei.project_name, ei.package_name, prcnt.TimeStart_PercLower, prcnt.TimeStart_PercUpper, @DaysBack, GETDATE())
+            SELECT       --TOP 1 
+                         ei.project_name AS [ProjectName]
+                        ,ei.package_name AS [PackageName]
+                        ,CONVERT(TIME(0), DATEADD(SECOND, AVG(DATEDIFF(SECOND, 0, CAST(ei_hst_av_strt.start_time AS TIME(0)))), 0)) AS [StartTimeAvg] -- so let's calculate the StartTime AVG no matter if the package runs once daily or not
+            
+            FROM        [SSISDB].internal.execution_info ei_hst_av_strt WITH (NOLOCK)
+            WHERE       1 = 1
+            AND         ei_hst_av_strt.project_name = ei.project_name
+            AND         ei_hst_av_strt.package_name = ei.package_name
+            AND         ei_hst_av_strt.status = 7 /* Success (we want to calculate stats on successfull runs only)   */ 
+            AND         ei_hst_av_strt.start_time >= DATEADD(DAY, @DaysBack, CAST(CAST(@EndDate+1 AS DATE) AS DATETIME)) /* Cast the date to midnight next day minus @DaysBack */ 
+            AND         ei_hst_av_strt.end_time < CAST(CAST(@EndDate+1 AS DATE) AS DATETIME) /* Cast the date to midnight next day */
+            /* Here we are excluding start-time outliers: */
+            AND         CAST(ei_hst_av_strt.start_time AS TIME(0)) >= prcnt.TimeStart_PercLower
+            AND         CAST(ei_hst_av_strt.start_time AS TIME(0)) <= prcnt.TimeStart_PercUpper
             )   AS      hstavg_start_time
 
 WHERE      ei.status = 2 -- Currently Running
